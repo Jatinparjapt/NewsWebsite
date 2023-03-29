cadda1f2c68b4ea9e4e91cc81da8c6b4655177c4
jatinkumar <jatin3961jk@gmail.com>
2023-03-29 21:00:15 +0530

    corsUpdate

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv")
+const cors = require("cors")
 dotenv.config();
 const app = express();
+app.use(cors())
 app.use(express.json())
 require("./mongoose/connection")
 app.use(require("./Routes/route"))
