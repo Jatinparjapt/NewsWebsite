cc272a31955d4261a7f6ad7c45c76ab27fa1928b
jatinkumar <jatin3961jk@gmail.com>
2024-01-06 13:10:26 +0530

    js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv")
 const cors = require("cors")
+const PORT = process.env.PORT || 5000;
 dotenv.config();
 const app = express();
 app.use(cors())
@@ -9,8 +10,7 @@ app.use(express.json())
 require("./mongoose/connection")
 
 app.use(require("./Routes/route"))
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , "127.0.0.1", ()=>{
+app.listen(PORT , ()=>{
     console.log(`Server Started on Port No. ${PORT}`);
 })
